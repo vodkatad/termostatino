14cd1a9a57851d77a5c0f969ef9041fb29696fd9
Elena Grassi <grassi.e@gmail.com>
2016-04-19 10:51:57 +0200

    Untested web client with POST to send temperature

--- termostatino.ino
+++ termostatino.ino
@@ -1,61 +1,108 @@
-#include <ICMPPing.h>
+#include <ICMPPing.h> 
+// is this needed to receive ping also? I do not think so, check.
 #include <util.h>
 #include <SPI.h>
 #include <Ethernet.h>
-#include "dht11.h"
+#include <dht11.h>
 #include <LiquidCrystal.h>
 #define DHTPIN 8    
+
+// Temperature sensor
 dht11 dht;
-int tempSoglia=20;
+const int tempSoglia = 20;
 //LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-// 10-13 are used by Ethernet
+// 10-13 are used by the eth shield.
 LiquidCrystal lcd(9, 6, 5, 4, 3, 2);
+// pin used for the relay. Do we need it?
 const int relay = 7;
+// Auto-assigned ip address and mac.
 byte mac[] = { 0x90, 0xA2, 0xDA, 0x00, 0x23, 0x5D }; 
 byte ip[] = { 130, 192, 140, 199 };
-char Data_RX;
-//Server ArduinoServer(80);
-void setup() {
-    Serial.begin(9600);
+// Web client and server.
+EthernetClient client;
+EthernetServer server;
+
+void setup() 
+{
+	Serial.begin(9600);
     lcd.begin(16, 2);
-    pinMode(relay,OUTPUT);
-    int lcdp = lcd.print("Setup");
-    //inizializza lo shield con il mac e l’ip
-    Ethernet.begin(mac, ip);
-    //inizializza l’oggetto server
-    //ArduinoServer.begin();
+    pinMode(relay, OUTPUT);
+    lcd.print("Setup");
+    // We connect.
+    Ethernet.begin(mac, ip); //dns, gateway, subnet
 }
 
-void loop() {
-    int lcdp = lcd.print("Loop");
-    Serial.println("loop");
-    Serial.println(lcdp);
-    dht.read(DHTPIN);
-    //float h = dht.readHumidity();
-    //float t = dht.readTemperature();
+void loop() 
+{
+    int chk = dht.read(DHTPIN);
     float h = dht.humidity;
     float t = dht.temperature;
-    if (isnan(t) || isnan(h)) {
-      Serial.println("Failed to read from DHT");
-    } else {
-      lcd.setCursor(0,0);
-      lcd.print("Temp=");
-      lcd.print(t);
-      lcd.print(" *C");
-      lcd.setCursor(0,1);
-      lcd.print("Humidity=");
-      lcd.print(h);
-      lcd.print("% ");
-      Serial.println("PRe if");
-      if(t > tempSoglia) {
-        Serial.println("if 1");
-        digitalWrite(relay, HIGH);
-      } else {
-        Serial.println("else");
-        digitalWrite(relay, LOW);
-      }
-      delay(5000);
-   }
+	if (isnan(t) || isnan(h)) {
+    	Serial.println("Failed to read from DHT");
+	    switch (chk) {
+		    case DHTLIB_OK: 
+				Serial.println("The sensor is misbehaving seriously"); 
+				break;
+		    case DHTLIB_ERROR_CHECKSUM: 
+				Serial.println("Checksum error"); 
+				break;
+		    case DHTLIB_ERROR_TIMEOUT: 
+				Serial.println("Time out error"); 
+				break;
+		    default: 
+				Serial.println("Unknown error"); 
+				break;
+	} else {
+		lcd.setCursor(0,0);
+	    lcd.print("Temp=");
+	    lcd.print(t);
+	    lcd.print(" *C");
+	    lcd.setCursor(0,1);
+	    lcd.print("Humidity=");
+	    lcd.print(h);
+	    lcd.print("% ");
+   		if(t > tempSoglia) {
+   	    	digitalWrite(relay, HIGH);
+			int chk_m = sendMail(t);
+			if (!chk_m) {
+				Serial.println("Failed to send mail");
+			}
+      	} else {
+        	digitalWrite(relay, LOW);
+      	}
+	//sendHeartbeat();
+	// We read a value every 10 seconds.
+	delay(10000);
+	}
 }
 
 
+/* Can we use a server inside arduino or not? Having the server waiting is not an option...
+int sendHeartbeat()
+{
+}
+*/
+
+int sendMail(temp)
+{
+	IPAddress web_server(130, 192, 147, 6);
+	if (client.connect(server, 80)) {
+		// Make a HTTP POST:
+    	client.println("POST /termostatino/send_mail.php HTTP/1.1");           
+	    client.println("Host: 130.192.140.199"); // bad hardcoded
+    	client.println("Content-Type: application/x-www-form-urlencoded");
+    	client.println("Connection: close");
+	    client.println("User-Agent: Arduino/1.0");
+	    client.print("Content-Length: ");
+	    client.println(temp.length());
+	    client.println();
+	    client.print(String("temp=" + temp));
+	    client.println();                                           
+		// http://forum.arduino.cc/index.php?topic=155218.0
+		return(1);
+	} else {
+		// if you didn't get a connection to the server:
+   		Serial.println("connection with web server failed");
+		return(0);
+  }
+}
